6838d42ce8364a7236ba04043f92afc51a035063
Petar Cholakov <petar.cholakov30@gmail.com>
2024-04-20 15:25:29 +0100

    changed api calls in service

--- service.js
+++ service.js
@@ -1,14 +1,11 @@
 import { BASE_URL, API_KEY } from "./constants.js"
 
-export const currentWeather = (city) => {
-
-    fetch(`${BASE_URL}/current.json?key=${API_KEY}&q=${city}`)
+export const currentWeather = async (city) => {
+    return fetch(`${BASE_URL}/current.json?key=${API_KEY}&q=${city}`)
         .then(res => res.json())
-        .then(result => console.log(result));
 }
 
-export const forecast = (city) => {
-    fetch(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}`)
+export const forecast = async (city) => {
+    return fetch(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}`)
         .then(res => res.json())
-        .then(result => console.log(result))
 }
