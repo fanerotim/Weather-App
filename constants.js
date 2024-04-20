f85cd419271883a8b1287d059370c1287036cfda
Petar Cholakov <petar.cholakov30@gmail.com>
2024-04-20 06:09:08 +0100

    setting up constants file to hold constants

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-const BASE_URL = 'http://api.weatherapi.com/v1'
-const API_KEY = 'http://api.weatherapi.com/v1'
+export const BASE_URL = 'http://api.weatherapi.com/v1'
+export const API_KEY = 'fe833299c1ae47b7a1162759241904'
