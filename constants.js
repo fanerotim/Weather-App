8f8feee9351246c647c7fa17ab8a88ebf2d07a24
Petar Cholakov <petar.cholakov30@gmail.com>
2024-04-22 11:56:59 +0100

    setting up initial html layout

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://api.weatherapi.com/v1'
-export const API_KEY = 'fe833299c1ae47b7a1162759241904'
+export const BASE_URL = 'http://api.weatherapi.com/v1';
+export const API_KEY = 'fe833299c1ae47b7a1162759241904';
--- controller.js
+++ controller.js
@@ -5,5 +5,7 @@ export const provideData = async (cityInput) => {
     const forecast = await httpService.getForecast(cityInput);
     const current = await httpService.getCurrentWeather(cityInput);
 
+    console.log('forecast', forecast);
+    console.log('current', current)
     renderData(forecast, current);
 }
