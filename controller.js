5d331578cb4eb5a3c597e7004a5be5f0d0d0cef3
Petar Cholakov <petar.cholakov30@gmail.com>
2024-04-26 09:40:51 +0100

    removing unnecessary log

--- controller.js
+++ controller.js
@@ -10,14 +10,14 @@ export const provideData = async (cityInput) => {
 
     renderData(forecast, current);
 
-    // getting the forecast only  for the next hours (excluding prior hours)
+    // getting the forecast only for the next hours (excluding prior hours)
     const currentTime = forecast.current.last_updated;
     const hourlyForecast = forecast.forecast.forecastday[0].hour;
+
     let nextHoursForecast = hourlyForecast
     .filter((date, i) => date.time > currentTime && i % 2 === 1)
 
     const conditions = imageFinder(nextHoursForecast.slice(0, 6));
-
-    console.log('conditions', conditions)
+    
     hourlyTemp(nextHoursForecast.slice(0, 6), conditions)
 }
