7bce2a0801382a8271bcb94dd27c2028d482966a
Petar Cholakov <petar.cholakov30@gmail.com>
2024-04-29 08:56:16 +0100

    started to clean up render component
    it will eventually be removed completely

--- render.js
+++ render.js
@@ -17,16 +17,4 @@ export function renderData(forecast, current) {
     
     dateEl.textContent = date;
     timeEl.textContent = time;
-
-    //TEMPERATURE
-    let tempetarure = document.querySelector('.temperature');
-    tempetarure.textContent = `${current.current.temp_c}°C`
-
-    //WIND
-    let wind = document.querySelector('.wind');
-    wind.textContent = `${current.current.wind_kph} km/h`
-
-    // for (let i = 0; i < hourUl.children.length; i++) {
-    //     hourUl.children[i].textContent = `${hourlyForecast[i].time} - ${hourlyForecast[i].temp_c}c`
-    // }
 }
