4b5de9e383f1a26c536b71cc83b6abb33a08dd41
Petar Cholakov <petar.cholakov30@gmail.com>
2024-04-29 08:57:42 +0100

    set up user form functionality

--- controller.js
+++ controller.js
@@ -1,8 +1,10 @@
 import * as httpService from "./service.js"
 import { renderData } from "./render.js";
 import { imageFinder } from "./utils/imageFinder.js";
+import { windFinder } from "./utils/windFinder.js";
 
 import {hourlyTemp} from "./views/hourlyTemperature.js"
+import { userForm } from "./views/userForm.js";
 
 export const provideData = async (cityInput) => {
     const forecast = await httpService.getForecast(cityInput);
@@ -10,6 +12,8 @@ export const provideData = async (cityInput) => {
 
     renderData(forecast, current);
 
+
+    // HOURLY TEMPERATURE FEATURE CODE BELOW
     // getting the forecast only for the next hours (excluding prior hours)
     const currentTime = forecast.current.last_updated;
     const hourlyForecast = forecast.forecast.forecastday[0].hour;
@@ -17,7 +21,16 @@ export const provideData = async (cityInput) => {
     let nextHoursForecast = hourlyForecast
     .filter((date, i) => date.time > currentTime && i % 2 === 1)
 
+    //returns an array of weather conditions as string
     const conditions = imageFinder(nextHoursForecast.slice(0, 6));
-    
     hourlyTemp(nextHoursForecast.slice(0, 6), conditions)
+    // END OF HOURLY TEMPERATURE CODE
+
+
+    // USER FORM CODE BELOW
+    //returns the direction of the wind as string
+    const windDirection = windFinder(forecast);
+    userForm(forecast, windDirection);
+    // END OF USER FORM CODE
+    
 }
