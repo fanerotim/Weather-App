f9f2d9637614c8cc35cc34ab86ed7d54f8a9aa3d
Petar Cholakov <petar.cholakov30@gmail.com>
2024-05-02 08:37:38 +0100

    improved background image helper function

--- utils/backgroundImageProvider.js
+++ utils/backgroundImageProvider.js
@@ -1,16 +1,18 @@
 export const backgroundImageProvider = (currentWeather) => {
-    const condition = currentWeather.current.condition.text;
+    const condition = currentWeather.current.condition.text.toLowerCase();
     console.log(condition);
     let conditionName = '';
 
-    if (condition.includes('cloudy') || condition.includes('Overcast')) {
+    if (condition.includes('cloudy') || condition.includes('overcast')) {
         conditionName = 'overcast'
-    } else if (condition.includes('Sunny') || condition.includes('Clear')) {
+    } else if (condition.includes('sunny') || condition.includes('clear')) {
         conditionName = 'sunny'
-    } else if (condition.includes('Thundery')) {
+    } else if (condition.includes('thundery')) {
         conditionName = 'thundery'
     } else if (condition.includes('rain') || condition.includes('drizzle')) {
         conditionName = 'rainy'
+    } else if (condition.includes('mist')) {
+        conditionName = 'mist';
     }
     
     const container = document.querySelector('.container');
