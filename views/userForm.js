6e103f19c099c09bbdb62dca25ea457f0c647e92
Petar Cholakov <petar.cholakov30@gmail.com>
2024-05-14 07:30:45 +0100

    setting up next days forecast markup

--- views/userForm.js
+++ views/userForm.js
@@ -1,6 +1,7 @@
 import {html, render} from "../node_modules/lit-html/lit-html.js";
 
 export function userForm(forecast, windDirection) {
+    console.log('user-form', forecast)
     const formTemplate = () => html `
             <label for="cityInput">Location</label><br>
             <input type="text" placeholder="Search..." value="Plovdiv" id="cityInput" name="cityInput" autocomplete="off">
@@ -9,15 +10,31 @@ export function userForm(forecast, windDirection) {
 
             <section class="temp-and-wind">
                 <h1 class="temperature">${forecast.current.temp_c}°C</h1>
-                <h2 class="wind"><span class='wind-direction'>${windDirection}</span> ${forecast.current.wind_kph}km/h</h2>
+                <p class='feels-like'>Feels like ${forecast.current.feelslike_c}°C</p>
+                <h2 class="wind"><span class='wind-direction'>${windDirection}</span>, ${forecast.current.wind_kph} km/h</h2>
             </section>
             
 
             <ul class="next-days">
-                <p>The Next Days Forecast</p>
-                <li></li>
-                <li></li>
-                <li></li>
+                <p class="heading">Next 3 Days Forecast</p>
+                
+                ${forecast.forecast.forecastday.map(data => html `
+        
+                <section class="next-days-data">
+                    <img class="weather-img" src="../assets/day/${data.day.condition.text.trim()}.png">
+
+                    <div class="date-and-condition">
+                        <p>${data.date}</p>
+                        <p>${data.day.condition.text}</p>
+                    </div>
+
+                    <div class="max-and-min-temp">
+                        <p>${data.day.maxtemp_c}</p>
+                        <p>${data.day.mintemp_c}</p>
+                    </div>
+
+                </section>`)}
+                
             </ul>`
 
     const formTemplateView = formTemplate();
